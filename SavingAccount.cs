9dc2b4ccfecd8da890ee0135a11112c48d5216b0
yousif123799 <141569188+yousif123799@users.noreply.github.com>
2025-03-12 17:52:25 +0200

    Add files via upload

---
+++ SavingAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Bank_system.Account;
+
+namespace Bank_system
+{
+    internal class SavingAccount: AAccount
+    {
+        private decimal InterestRate;  
+
+        public SavingAccount(int accountNumber, string ownerName, decimal initialBalance, decimal interestRate): base(accountNumber, ownerName, initialBalance)
+        {
+            InterestRate = interestRate;
+        }
+        public  void ApplyAccountRules()
+        {
+            
+            decimal interest = balance * InterestRate / 100;
+            deposit(interest);
+        }
+
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_system
+{
+    internal class Transaction
+    {
+        public string type { get; set; }
+        public decimal amount { get; set; }
+        public DateTime DateTime { get; set; }
+        public Transaction(string type, decimal amount)
+        {
+            DateTime = DateTime.Now;
+            this.type = type;
+            this.amount = amount;
+
+        }
+        public string operation()
+        {
+            return $"Transaction type: {type}, Amount: {amount}, Date: {DateTime.ToShortDateString()}";
+        }
+
+
+    }
+}
